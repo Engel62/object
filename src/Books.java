4dbf8370b1f7a69366b29fcca4aec57ecdc2257a
Engel62 <sr.engel@bk.ru>
2022-11-22 22:18:50 +0300

    домашнее задание готово

---
+++ src/Books.java
@@ -0,0 +1,24 @@
+public class Books {
+   private final String bookTitle;
+    private final Author author;
+    private int yearOfPublication;
+
+    public Books(String bookTitle, Author author, int yearOfPublication) {
+        this.bookTitle = bookTitle;
+        this.author = author;
+        this.yearOfPublication = yearOfPublication;
+        }
+    public String getBookTitle() {
+        return bookTitle;
+    }
+    public int getYearOfPublication() {
+        return yearOfPublication;
+    }
+    public Author getAuthor() {
+        return author;
+    }
+    public void setYearOfPublication(int yearOfPublication) {
+        this.yearOfPublication = yearOfPublication;
+    }
+}
+
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        Person sarah = new Person("Sarah", 30);
-        System.out.println("sarah.name = " +  sarah.getName());
-        System.out.println("sarah.age = " + sarah.getAge());
-        sarah.setAge(31);
-        System.out.println("sarah.getAge() = " + sarah.getAge());
-        Person john = new Person("John", 13);
-
+        Author author = new Author("Дмитрий","Глуховский");
+        Author author1 = new Author("Александр", "Мазин");
+        Books book = new Books("Метро 2033", author, 2005);
+        Books book1 = new Books("Варяг", author1, 2001);
+        System.out.println(author.getName() + " " +author.getSurname() + " <<" + book.getBookTitle() + ">> , " + book.getYearOfPublication());
+        System.out.println(author1.getName() + " " +author1.getSurname() + " <<" + book1.getBookTitle() + ">>, " + book1.getYearOfPublication());
+        book1.setYearOfPublication(2002);
+        System.out.println(book1.getYearOfPublication());
     }
 }
